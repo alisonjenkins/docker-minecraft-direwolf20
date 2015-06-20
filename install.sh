0eb16a647d9f0cbdbf5a5995501af510b4d8b300
root <root@ns516374.ip-192-99-32.net>
2015-06-20 18:43:03 -0400

    Fix install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-mkdir -p /srv/minecraft/agrarianskies2/world
-mkdir -p /srv/minecraft/agrarianskies2/logs
-touch /srv/minecraft/agrarianskies2/banned-ips.json
-touch /srv/minecraft/agrarianskies2/banned-players.json
-touch /srv/minecraft/agrarianskies2/ops.json
-touch /srv/minecraft/agrarianskies2/usercache.json
-touch /srv/minecraft/agrarianskies2/whitelist.json
+mkdir -p /srv/minecraft/direwolf20/world
+mkdir -p /srv/minecraft/direwolf20/logs
+touch /srv/minecraft/direwolf20/banned-ips.json
+touch /srv/minecraft/direwolf20/banned-players.json
+touch /srv/minecraft/direwolf20/ops.json
+touch /srv/minecraft/direwolf20/usercache.json
+touch /srv/minecraft/direwolf20/whitelist.json
